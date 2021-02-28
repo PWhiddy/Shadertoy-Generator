02b4ffab76b6adfa0a1a9ee0a1eddfd129342997
PWhiddy <all.cows.like.to.moo@gmail.com>
2021-02-28 04:28:24 +0000

    cleaner and better progress output

--- download_shaders.py
+++ download_shaders.py
@@ -1,31 +1,32 @@
 import urllib.request
 import json
 import time
+from tqdm import tqdm
 
 def getTotal():
     with open("AllShadertoyShaders.json") as shaders_json:
         shaders = json.load(shaders_json)
         return int(shaders['Shaders'])
 
-def readIndex(idx):
-    with open("AllShadertoyShaders.json") as shaders_json:
-        with open("key.txt") as key:
-            api_key = key.read().replace('\n', '')
-            shaders = json.load(shaders_json)
-            first = shaders['Results'][idx]
-            source = urllib.request.urlopen("https://www.shadertoy.com/api/v1/shaders/"+first+"?key="+api_key).read()
-            return source
-            #sd = json.loads(source.decode("utf-8"))
-            #return sd
+def readIndex(idx, shaders, api_key):                    
+    first = shaders['Results'][idx]
+    source = urllib.request.urlopen("https://www.shadertoy.com/api/v1/shaders/"+first+"?key="+api_key).read()
+    return source
+    #sd = json.loads(source.decode("utf-8"))
+    #return sd
 
 if __name__ == "__main__":
     num = getTotal()
     print(num)
     # this can be done in batches
-    for i in range(0,num):
-        if (i%100 == 0):
-            print(i)
-        with open("allshaders/s" + str(i) + ".json", 'wb') as output:
-            output.write(readIndex(i))
-            output.close()
-        time.sleep(0.1)
+    with open("key.txt") as key:
+        api_key = key.read().replace('\n', '')
+        with open("AllShadertoyShaders.json") as shaders_json:
+            shaders = json.load(shaders_json)
+            for i in tqdm(range(0,num)):
+                shad = readIndex(i, shaders, api_key)
+                shady = json.loads(shad.decode('utf-8'))
+                with open("allshaders/" + shady['Shader']['info']['id'] + ".json", 'wb') as output:
+                    output.write(shad)
+                    output.close()
+                time.sleep(0.01)
